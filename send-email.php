addf5bf10932601f8281af7121e838d0a1203a85
Manasi Srivastava <manasisrivastava7@gmail.com>
2021-02-28 09:22:22 +0530

    Add files via upload

---
+++ send-email.php
@@ -0,0 +1,35 @@
+require 'class.phpmailer.php';
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Mailer = 'smtp';
+$mail->SMTPAuth = true;
+$mail->Host = 'smtp.gmail.com'; // "ssl://smtp.gmail.com" didn't work
+$mail->Port = 465;
+$mail->SMTPSecure = 'ssl';
+// or try these settings (worked on XAMPP and WAMP):
+// $mail->Port = 587;
+// $mail->SMTPSecure = 'tls';
+ 
+ 
+$mail->Username = "msprojects7@gmail.com";
+$mail->Password = "clmsem77";
+ 
+$mail->IsHTML(true); // if you are going to send HTML formatted emails
+$mail->SingleTo = true; // if you want to send a same email to multiple users. multiple emails will be sent one-by-one.
+ 
+$mail->From = "msprojects7@gmail.com";
+$mail->FromName = "Your Name";
+ 
+$mail->addAddress("user.1@yahoo.com","User 1");
+$mail->addAddress("user.2@gmail.com","User 2");
+ 
+$mail->addCC("user.3@ymail.com","User 3");
+$mail->addBCC("user.4@in.com","User 4");
+ 
+$mail->Subject = "Testing PHPMailer with localhost";
+$mail->Body = "Hi,<br /><br />This system is working perfectly.";
+ 
+if(!$mail->Send())
+    echo "Message was not sent <br />PHPMailer Error: " . $mail->ErrorInfo;
+else
+    echo "Message has been sent";
